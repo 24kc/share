bed57c93af639f874c42bea4beda0d472a7c119b
cpplay <2641797006@qq.com>
2019-12-18 09:38:03 +0800

    -

---
+++ cpp/getInstance/singleton.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+class T {
+  public:
+	static T* (*getInstance)();
+	friend ostream& operator<< (ostream&, const T&);
+
+  protected:
+	T();
+	T(const T&) = delete;
+	T& operator= (const T&) = delete;
+	~T() = default;
+
+  private:
+	string data;
+	static T* instance;
+
+	static T* f1();
+	static T* f2();
+};
+
+T* (*T::getInstance)() = T::f1;
+T* T::instance;
+
+T::T()
+{
+	cout<< "T()" <<endl;
+	data = "ABC傻逼";
+}
+
+T*
+T::f1()
+{
+	instance = new T();
+	getInstance = f2;
+	return getInstance();
+}
+
+T*
+T::f2()
+{
+	return instance;
+}
+
+ostream&
+operator<< (ostream& out, const T& t)
+{
+	out<<t.data;
+	return out;
+}
+
+int main()
+{
+	T *t = T::getInstance();
+	T *t1 = T::getInstance();
+	cout<<*t<<endl;
+	cout<<*t1<<endl;
+}
+
