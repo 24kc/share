41576a0e0e07e5b2b1e8469359e028d56b703c16
cpplay <2641797006@qq.com>
2019-12-19 13:56:25 +0800

    -

--- c/template/list/2.0/test.c
+++ c/template/list/2.0/test.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/* 设置元素类型为int */
 #define type int
 #include "list.h"
 
 int cmp(const int *p1, const int *p2)
 {
 	return (*p1 > *p2) - (*p1 < *p2);
+/*
+	比较函数, 从小到大排序
+	改成 return (*p1 < *p2) - (*p1 > *p2); 则从大到小排序
+*/
 }
 
 int main() {
@@ -14,11 +19,13 @@ int main() {
 
 	list L;
 
-	list_init(&L);
+	list_init(&L); /* 初始化链表 */
 
+	/* 把数据插入到链表中 */
 	for (i=0; i<10; ++i)
 		list_push_back(&L, &a[i]);
 
+	/* 遍历输出链表 */
 	p = list_first(&L);
 	while (p != list_tail(&L)) {
 		printf("%d ", *p);
@@ -26,8 +33,10 @@ int main() {
 	}
 	printf("\n");
 
+	/* 根据cmp比较函数给链表排序 */
 	list_sort(&L, cmp);
 
+	/* 遍历输出链表 */
 	p = list_first(&L);
 	while (p != list_tail(&L)) {
 		printf("%d ", *p);
