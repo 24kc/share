620ce60ea989dc1be3b02d61b0ae91b0d7517999
24kc <24kc@>
2019-12-31 09:13:30 +0800

    -

--- swp/001.c
+++ swp/001.c
@@ -1,20 +1,42 @@
 #include <stdio.h>
-
-void change(int[2]);
+#include <stdlib.h>
 
 int main()
 {
-	int a[2] = {24, 'k'};
-	change(a);
-	printf("%d %d\n", a[0], a[1]);
-	return 0;
+	int i,j,n,t,z;
+	printf("请输入数组个数: ");
+	scanf("%d",&n);
+	int *a = (int*)malloc(sizeof(int) * (n+1)); // int a[n+1]
+	printf("输入数组: ");
+	for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	//a[n]=0;
+	for(i=0;i<n-1;i++)
+	{
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t=a[j],a[j]=a[j+1],a[j+1]=t;
+			}
+		}
+	}
+	printf("排序后的数组: ");
+	for(i=0;i<n;i++)
+		printf("%d ",a[i]);
+	printf("\n");
+	printf("请输入要插入的数字: ");
+	scanf("%d",&z);
+	for(i=0;i<n;i++)
+	{
+		if(z < a[i])
+			break;
+	}
+	for (j=n; j>i; --j)
+		a[j] = a[j-1];
+	a[i] = z;
+	printf("插入后的数组: ");
+	for(i=0;i<n+1;i++)
+		printf("%d ",a[i]);
+	printf("\n");
 }
-
-void
-change(int a[2])
-{
-	int tmp = a[0];
-	a[0] = a[1];
-	a[1] = tmp;
-}
-
