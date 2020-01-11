2b3954c9f05575750e070b67835fc7f068ca69b3
24kc <24kc@>
2020-01-11 12:13:21 +0800

    -

--- swp/001.c
+++ swp/001.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-typedef struct{
-	int A, B, C, s, b;
-	char *p;
-}STU;
-
 int main()
 {
-	STU *stu = NULL;
-	printf("%p\n", (void*)&stu->p);
+	char c = -128;
+	unsigned char uc = -128;
+	printf(" c>>1 = %d\n", c>>1);
+	printf("uc>>1 = %d\n", uc>>1);
 	return 24-'k';
 }
 
