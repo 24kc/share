7ab902507261f0f825d5f0ed2bcd77b5d6509dd6
24kc <24kc@>
2020-04-02 14:39:45 +0800

    -

--- cpp/thread_pool/001.cpp
+++ cpp/thread_pool/001.cpp
@@ -4,8 +4,10 @@
 #include "thread_pool.h"
 using namespace std;
 
+// 测试函数
 string f(int n)
 {
+	// 休眠n个0.1秒
 	this_thread::sleep_for(chrono::milliseconds(n*100));
 	string s = "ABC";
 	for (int i=0; i<n; ++i)
@@ -15,6 +17,7 @@ string f(int n)
 
 int main()
 {
+	// 线程池, 4个线程
 	akm::thread_pool<4> pool;
 
 	constexpr int N = 10;
@@ -24,11 +27,13 @@ int main()
 
 	for (int i=0; i<N; ++i)
 		s[i] = pool.push(f, i+1);
+	// 向线程池提交N个任务
 
 	cout<<"push "<<N<<" tasks, waiting..."<<endl;
 
 	for (int i=0; i<N; ++i)
 		s[i].wait();
+	// 等待任务完成
 
 	clock_t t1 = clock();
 
@@ -36,6 +41,7 @@ int main()
 
 	for (int i=0; i<N; ++i)
 		cout<<s[i].get()<<endl;
+	// 获取计算结果并输出
 
 	return 24-'k';
 }
