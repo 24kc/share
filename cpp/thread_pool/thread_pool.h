ba68ad8c0b73d3dc5b6d9af6a7f1ff03e75e9e5c
24kc <24kc@>
2020-04-17 11:49:16 +0800

    -

--- cpp/thread_pool/thread_pool.h
+++ cpp/thread_pool/thread_pool.h
@@ -15,10 +15,17 @@ class thread_pool;
 template<size_t N>
 class thread_pool_base {
   private:
-	thread_pool_base() = default;
+	thread_pool_base();
 	thread_pool_base(const thread_pool_base&) = delete;
 	thread_pool_base& operator= (const thread_pool_base&) = delete;
-	~thread_pool_base() = default;
+	~thread_pool_base();
+
+	template<class F, class... Args>
+	void thread(F&& f, Args&&... args);
+	// 向线程池提交任务
+
+	void join();
+	// 等待所有已提交任务完成
 
   private:
 	void thread_loop();
@@ -41,51 +48,17 @@ class thread_pool_base {
 }; // class thread_pool_base
 
 template<size_t N>
-class thread_pool {
-  public:
-	thread_pool();
-	thread_pool(const thread_pool&) = delete;
-	thread_pool& operator= (const thread_pool&) = delete;
-	~thread_pool();
-
-	template<class F, class... Args>
-	void thread(F&& f, Args&&... args);
-	// 向线程池提交任务
-
-	void join();
-	// 等待所有已提交任务完成
-
-  private:
-	thread_pool_base<N> *pool;
-
-	static constexpr int STOP = thread_pool_base<N>::STOP;
-	static constexpr int JOIN = thread_pool_base<N>::JOIN;
-}; // class thread_pool
-
-
-template<size_t N>
-thread_pool<N>::thread_pool()
+thread_pool_base<N>::thread_pool_base()
 {
-	pool = new thread_pool_base<N>();
-
-	auto& threads = pool->threads;
-	auto& flags = pool->flags;
-	auto& nexit = pool->nexit;
-
 	flags = 0;
 	nexit = 0;
 	for (size_t i=0; i<N; ++i)
-		threads[i] = std::move(std::thread(&thread_pool_base<N>::thread_loop, pool));
+		threads[i] = std::move(std::thread(&thread_pool_base<N>::thread_loop, this));
 }
 
 template<size_t N>
-thread_pool<N>::~thread_pool()
+thread_pool_base<N>::~thread_pool_base()
 {
-	auto& threads = pool->threads;
-	auto& mutex = pool->mutex;
-	auto& condition = pool->condition;
-	auto& flags = pool->flags;
-
 	std::lock_guard<std::mutex> lock(mutex);
 	for (size_t i=0; i<N; ++i)
 		threads[i].detach();
@@ -97,28 +70,19 @@ thread_pool<N>::~thread_pool()
 template<size_t N>
 template<class F, class... Args>
 void
-thread_pool<N>::thread(F&& f, Args&&... args)
+thread_pool_base<N>::thread(F&& f, Args&&... args)
 {
 	{
-		auto& mutex = pool->mutex;
-		auto& tasks = pool->tasks;
 		std::lock_guard<std::mutex> lock(mutex);
-		tasks.emplace(std::bind(f, args...));
+		tasks.emplace(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
 	}
-	auto& condition = pool->condition;
 	condition.notify_one();
 }
 
 template<size_t N>
 void
-thread_pool<N>::join()
+thread_pool_base<N>::join()
 {
-	auto& mutex = pool->mutex;
-	auto& condition = pool->condition;
-	auto& cv_join = pool->cv_join;
-	auto& flags = pool->flags;
-	auto& nfree = pool->nfree;
-
 	std::unique_lock<std::mutex> lock(mutex);
 
 	nfree = 0;
@@ -163,6 +127,31 @@ thread_pool_base<N>::thread_loop()
 		task();
 	}
 }
+
+template<size_t N>
+class thread_pool {
+  public:
+	thread_pool(): pool_base(new thread_pool_base<N>()) {};
+	thread_pool(const thread_pool&) = delete;
+	thread_pool& operator= (const thread_pool&) = delete;
+	~thread_pool() = default;
+
+	template<class F, class... Args>
+	void thread(F&& f, Args&&... args)
+	{
+		pool_base->thread(std::forward<F>(f), std::forward<Args>(args)...);
+	}
+	// 向线程池提交任务
+
+	void join()
+	{
+		pool_base->join();
+	}
+	// 等待所有已提交任务完成
+
+  private:
+	thread_pool_base<N> *pool_base;
+}; // class thread_pool
 
 } // namespace akm
 
